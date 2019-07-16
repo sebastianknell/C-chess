8717b4bb0cf8656d305ef09125fa442ac95858fe
Sebastian Knell <sebastianknell@hotmail.com>
2019-07-16 12:47:59 -0500

    Initial Commit

---
+++ chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chess
+//
+//  Created by Sebastian Knell on 7/16/19.
+//  Copyright © 2019 Sebastian Knell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
